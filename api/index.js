1c560d61504951ea5e5b9c3fa4124a038bc45a34
amaan35 <amaanlm35@gmail.com>
2024-03-12 19:43:40 +0530

    installing axios in frontend to send requests to backend server and installing cors in backend to allow network request from frontend

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const { Product } = require("./models/product");
 const app = express();
 app.use(express.json());
+app.use(cors());
 const port = 3001;
 
 dotenv.config();
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Homepage() {
+  const [productList, setProductList] = useState([]);
+  const getProducts = async () => {
+    try {
+      const res = await axios.get("http://localhost:3001/read");
+      setProductList(res.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
   return (
     <div>
       Homepage
+      <h2>{productList.length > 0 && productList[0].title}</h2>
     </div>
-  )
+  );
 }
