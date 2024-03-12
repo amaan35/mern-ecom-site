b92eb2d7c8d94dc61c9cc485b7ee6e7e6ca41f27
amaan35 <amaanlm35@gmail.com>
2024-03-12 17:44:10 +0530

    Created Product schema and model, created CRUD operations on Product data

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
+const { Product } = require("./models/product");
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 dotenv.config();
@@ -14,9 +16,61 @@ mongoose
   .catch((error) => {
     console.log("error connecting to database : ", error);
   });
+
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
+
+app.post("/create", async (req, res) => {
+  const newProduct = new Product({
+    title: req.body.title,
+    description: req.body.description,
+    price: req.body.price,
+    discountPercentage: req.body.discountPercentage,
+    rating: req.body.rating,
+    stock: req.body.stock,
+    brand: req.body.brand,
+    category: req.body.category,
+    thumbnail: req.body.thumbnail,
+    images: req.body.images,
+  });
+  await Product.create(newProduct);
+  res.status(200).json("Product added to the database");
+});
+
+app.get("/read", async (req, res) => {
+  const productList = await Product.find();
+  res.status(200).send(productList);
+});
+
+app.get("/read/:id", async (req, res)=>{
+    const product_id = req.params.id;
+    const product = await Product.findById(product_id);
+    res.status(200).json(product);
+})
+
+app.put("/update/:id", async (req, res) => {
+  const product_id = req.params.id;
+  await Product.findByIdAndUpdate(product_id, {
+    title: req.body.title,
+    description: req.body.description,
+    price: req.body.price,
+    discountPercentage: req.body.discountPercentage,
+    rating: req.body.rating,
+    stock: req.body.stock,
+    brand: req.body.brand,
+    category: req.body.category,
+    thumbnail: req.body.thumbnail,
+    images: req.body.images,
+  });
+  res.status(200).json("Product updated successfully");
+});
+
+app.delete("/delete/:id", async (req, res)=>{
+    const product_id = req.params.id;
+    await Product.findByIdAndDelete(product_id);
+    res.status(200).json("Product deleted successfully")
+})
 
 app.listen(port, () => {
   console.log(`server started at port : ${port}`);
